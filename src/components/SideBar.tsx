b131c410521a65591f247e8cc3e6ef39af675d53
kahmariano <kmariano138@gmail.com>
2021-11-11 22:08:54 -0300

    refatoracao SideBar e Content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,46 @@
-export function SideBar() {
-  // Complete aqui
+import {Button} from './Button'
+import { ButtonHTMLAttributes } from 'react';
+
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
 }
+
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+  iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  selected: boolean;
+}
+
+interface SideBarProps {
+  genres: GenreResponseProps[]
+  selectedGenreId:number
+  handleClickButton:(id:number)=> void;
+}
+
+
+export function SideBar({genres, handleClickButton, selectedGenreId}:SideBarProps) {
+  // Complete aqui
+return (
+  <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+)
+}
+
